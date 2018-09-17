59c5b7771ea040324ece531b6c262e165f95a18b
Bram van Heuvelen <bramvanheuvelen@Brams-MacBook-Pro.local>
2018-09-17 20:58:58 +0200

    list on screen

--- src/app/match/match.module.ts
+++ src/app/match/match.module.ts
@@ -6,11 +6,15 @@ import { MatchAddComponent } from './match-add/match-add.component';
 import { MatchListComponent } from './match-list/match-list.component';
 import { MatchItemComponent } from './match-item/match-item.component';
 import { MatchEditComponent } from './match-edit/match-edit.component';
+import { MatchService } from './match.service';
 
 @NgModule({
   imports: [
     CommonModule,
     MatchRoutingModule
+  ],
+  providers: [
+    MatchService
   ],
   declarations: [MatchAddComponent, MatchListComponent, MatchItemComponent, MatchEditComponent]
 })
